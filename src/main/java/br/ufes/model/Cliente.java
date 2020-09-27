80424013ff5b8c4551ed061a55b18b7733525df8
nandinhomutum <nandinhomutum@hotmail.com>
2020-09-27 13:17:54 -0300

    Criação das classes que faltam baseada no modelo do diagrama de classe.
    Interfaces: FormaPagamento e Desconto
    
    Classes: NotaFiscal, PagamentoAVista, PagamentoDebito, PagamentoCredito, DescontoAVista, Desconto3, DescontoAniversario, Endereço.

--- src/main/java/br/ufes/model/Cliente.java
+++ src/main/java/br/ufes/model/Cliente.java
@@ -1,9 +1,14 @@
 package br.ufes.model;
 
+import java.time.LocalDate;
+
 public final class Cliente {
 
     private final String nome;
     private final String CNPJOuCPF;
+    private double pontuacao;
+    private LocalDate aniversario;
+    private Endereco endereco;
 
     public Cliente(String nome, String codigo) {
         this.nome = nome;
